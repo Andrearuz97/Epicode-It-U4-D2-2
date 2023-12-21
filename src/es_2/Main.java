d385fec35ab58f1a36ec8aeb9d400d53e1e6bd50
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-12-22 00:01:06 +0100

    Update Main.java

--- src/es_2/Main.java
+++ src/es_2/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		sim.aggiornaCredito(10.0);
 		sim.effettuaChiamata("3478906782", 5);
 		sim.effettuaChiamata("3403838367", 2);
-		sim.effettuaChiamata("3207645123", 19);
+		sim.effettuaChiamata("3207645123", 16);
 		sim.effettuaChiamata("3336747352", 11);
 		sim.effettuaChiamata("3406677858", 6);
 		sim.stampaDatiSim();
